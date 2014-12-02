e758718d10fc2d14618e27cb8472f3137a53ab31
William <line2up@aliyun.com>
2014-12-02 16:54:20 +0800

    将波特率改为38400，可与51422通讯版本

--- STM32Cube_FW_F4_V1.3.0/Projects/STM32F4-Discovery/Templates/Src/main.c
+++ STM32Cube_FW_F4_V1.3.0/Projects/STM32F4-Discovery/Templates/Src/main.c
@@ -313,8 +313,8 @@ void MX_USART1_UART_Init(void)
 
   huart1.State = HAL_UART_STATE_RESET;//weizhong add 20141031
   huart1.Instance = USARTx;
-  huart1.Init.BaudRate = 9600;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.BaudRate = 38400;//9600;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;//UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
   huart1.Init.Mode = UART_MODE_TX_RX;
